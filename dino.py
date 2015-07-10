0812a8e6edfc87e028bbbb2d7ea42a2e9686f55a
Adam Thrash <cathrash92@gmail.com>
2015-07-10 16:27:45 -0500

    Dinosaur selection available
    Choices: stegasaurus, dimetredon

--- dino.py
+++ dino.py
@@ -1,40 +1,71 @@
-def print_steg_progress(number_of_items, current_position):
+def print_dino_progress(number_of_items, current_position, dinosaur):
     stegasaurus = """
-                                     .       .
-                                    / `.   .' \\
-                            .---.  <    > <    >  .---.
-                            |    \\\\  \\ - ~ ~ - /  /    |
-                             ~-..-~             ~-..-~
-                         \\~~~\\.'                    `./~~~/
-               .-~~^-.    \\__/                        \\__/
-             .'  O    \\     /               /       \\  \\
-            (_____,    `._.'               |         }  \\/~~~/
-             `----.          /       }     |        /    \\__/
-                   `-.      |       /      |       /      `. ,~~|
-                       ~-.__|      /_ - ~ ^|      /- _      `..-'   f: f:
-                            |     /        |     /     ~-.     `-. _||_||_
-                            |_____|        |_____|         ~ - . _ _ _ _ _>
+                         .       .
+                        / `.   .' \\
+                .---.  <    > <    >  .---.
+                |    \  \ - ~ ~ - /  /    |
+                 ~-..-~             ~-..-~
+             \~~~\.'                    `./~~~/
+              \__/                        \__/
+               /                  .-    .  \\
+        _._ _.-    .-~ ~-.       /       }   \/~~~/
+    _.-'q  }~     /       }     {        ;    \__/
+   {'__,  /      (       /      {       /      `. ,~~|   .     .
+    `''''='~~-.__(      /_      |      /- _      `..-'   \\\\   //
+                / \   =/  ~~--~~{    ./|    ~-.     `-..__\\\\_//_.-'
+               {   \  +\         \  =\ (        ~ - . _ _ _..---~
+               |  | {   }         \   \_\\
+              '---.o___,'       .o___,'
     """
 
-    steg_array = stegasaurus.split("\n")
+    dimetredon = """
+                                _._
+                              _/:|:
+                             /||||||.
+                             ||||||||.
+                            /|||||||||:
+                           /|||||||||||
+                          .|||||||||||||
+                          | ||||||||||||:
+                        _/| |||||||||||||:_=---.._
+                        | | |||||:'''':||  '~-._  '-.
+                      _/| | ||'         '-._   _:    ;
+                      | | | '               '~~     _;
+                      | '                _.=._    _-~
+                   _.~                  {     '-_'
+           _.--=.-~       _.._          {_       }
+       _.-~   @-,        {    '-._     _. '~==+  |
+      ('          }       \_      \_.=~       |  |
+      `,,,,,,,'  /_         ~-_    )         <_oo_>
+        `-----~~/ /'===...===' +   /
+               <_oo_>         /  //
+                             /  //
+                            <_oo_>
+    """
+    if dinosaur == "dimetredon":
+        dino_array = dimetredon.split("\n")
+    elif dinosaur == "stegasaurus":
+        dino_array = stegasaurus.split("\n")
+    else:
+        dino_array = stegasaurus.split("\n")
 
-    n_lines = number_of_items/16
+    n_lines = number_of_items/len(dino_array)
 
     if current_position == 0:
         dino_line = 0
     else:
-        dino_line = ((current_position/n_lines)-1)%16
+        dino_line = ((current_position/n_lines)-1)%len(dino_array)
     if current_position%n_lines == 0:
-        print(steg_array[dino_line])
-        if dino_line != len(steg_array)-1:
+        print(dino_array[dino_line])
+        if dino_line != len(dino_array)-1:
             dino_line += 1
         else:
             dino_line = 0
 
 ## EXAMPLE USAGE
-# stuff = [0]*100
-# for i in range(len(stuff)):
-#     # process your stuff
-#     stuff[i] = stuff[i]+i
-#     print_steg_progress(len(stuff), i)
-# print(stuff)
+stuff = [0]*100
+for i in range(len(stuff)):
+    # process your stuff
+    stuff[i] = stuff[i]+i
+    print_dino_progress(len(stuff), i, "stegasaurus")
+print(stuff)
